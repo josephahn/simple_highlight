926c8384ca6398dfa29a864d1504d2ddddb0571b
ta3hoon <ta3hoon@gmail.com>
2014-04-22 11:14:15 -0700

    refactor and add comments

--- content.js
+++ content.js
@@ -3,16 +3,14 @@ var highlightSpan = function() {
   span.className = "highlighted";
   span.style.backgroundColor = "yellow";
   return span;
-}
+};
 
 var highlight = function(anchorNode, anchorOffset, focusNode, focusOffset, range) {
-  console.log('ANCHOR === FOCUS: ' + (anchorNode === focusNode).toString());
   // selection is within same DOM node
   if (anchorNode === focusNode) {
-    range.surroundContents(span);
+    range.surroundContents(highlightSpan());
     range.detach();
   } else { // selection spans multiple DOM nodes
-    console.log('ERROR: PARTIAL SELECTED DOM NODES');
 
     // nodeList containing start and end container nodes
     var ancestorChildNodes = range.commonAncestorContainer.childNodes;
@@ -24,35 +22,22 @@ var highlight = function(anchorNode, anchorOffset, focusNode, focusOffset, range
       var recurseNodes = function(nodeList) {
 
         for (var i = 0; i < nodeList.length; i++) {
-          console.log('i: ' + i);
-          console.dir(nodeList[i]);
-
           if (nodeList[i].className !== "highlighted") {
             if (highlighted) {
               if (focusNode === nodeList[i]) {
-                console.log('FOUND END CONTAINER');
-                console.dir(nodeList[i]);
                 nodes.push(nodeList[i]);
                 highlighted = false;
               } else {
-                console.log('WHOLE DOM NODE');
-                console.log(nodeList[i]);
                 if (nodeList[i].nodeType === 3 && nodeList[i].textContent) {
-                  console.log('FOUND TEXT NODE');
-                  console.log(nodeList[i]);
                   nodes.push(nodeList[i]);
                 }
               }
             }
             if (anchorNode === nodeList[i]) {
-              console.log('FOUND START CONTAINER');
-              console.dir(nodeList[i]);
               nodes.push(nodeList[i]);
               highlighted = true;
             }
             if (nodeList[i].hasChildNodes()) {
-              console.log('found children... recursing through: ');
-              console.log(nodeList[i].childNodes);
               recurseNodes(nodeList[i].childNodes);
             }
           }
@@ -62,26 +47,21 @@ var highlight = function(anchorNode, anchorOffset, focusNode, focusOffset, range
       return nodes;
     };
 
+    // array of nodes to highlight
     var nodesToHighlight = highlightNodes();
     for (var i = 0; i < nodesToHighlight.length; i++) {
+      var newRange = document.createRange();
       if (i === 0) {
-        var newRange = document.createRange();
         newRange.setStart(nodesToHighlight[i], anchorOffset);
         newRange.setEnd(nodesToHighlight[i], nodesToHighlight[i].length);
-        newRange.surroundContents(highlightSpan());
-        newRange.detach();
       } else if (i === nodesToHighlight.length - 1) {
-        var newRange = document.createRange();
         newRange.setStart(nodesToHighlight[i], 0);
         newRange.setEnd(nodesToHighlight[i], focusOffset);
-        newRange.surroundContents(highlightSpan());
-        newRange.detach();
       } else {
-        var newRange = document.createRange();
         newRange.selectNode(nodesToHighlight[i]);
-        newRange.surroundContents(highlightSpan());
-        newRange.detach();
       }
+      newRange.surroundContents(highlightSpan());
+      newRange.detach();
     }
   }
 };
@@ -110,8 +90,7 @@ var getSelectionHtml = function() {
 
 $(function() {
   $('body').on('mouseup', function(){
-    var selection = getSelectionHtml();
-    console.log('SELECTED TEXT: ' + selection);
-    console.log('++++++++++++++++');
+    // var selection = getSelectionHtml();
+    getSelectionHtml();
   });
 });
